4e6ee6c84cf12834cc3c137699de078277841f64
echolox <echolox+bitbucket@gmail.com>
2017-04-22 22:59:07 +0200

    Cleans up and adds docstrings

--- controls.py
+++ controls.py
@@ -1,6 +1,11 @@
-from util import FULL, clip, dprint, iprint
+from util import clip, dprint, iprint
 
 class ControlParent(object):
+    """
+    All objects of the type Control and its subtypes need to be constructed with a
+    parent object of this type (or another type that provides the same methods. This
+    is Python, we don't enforce this, right?).
+    """
     
     def send_to_device(self, ID, value):
         """
@@ -19,8 +24,13 @@ class ControlParent(object):
 ### HARDWARE CONTROL SIMULATIONS
 
 class Control(object):
+    """
+    A Control "simulates" the controls you would find on a hardware devices. Things
+    like buttons, dials, sliders/faders etc. are controls.
+    """
 
-    configurable = ["minval", "maxval", "blink"]
+    # Designate which attributes of the Control can be configured by the user
+    # and provide some defaults
     default_conf = {"minval": 0, 
                     "maxval": 127,
                     "blink": False,
@@ -34,28 +44,28 @@ class Control(object):
         self.blink = blink
         self._value = 0
 
-    def unblink(self):
-        self.blink = False
-        self.parent.send_to_device(self.ID, 0)
-
     def get_value(self):
         return self._value
 
+    # @RENAME: set_value
     def value(self, value):
-        self._value = value
+        """
+        Try to set the control to the provided value. Due to the configuration of the
+        Control it might assume a different value. The assumed value is returned to
+        the caller.
+        """
+        self._value = clip(self.minval, self.maxval, value)
         return self._value
 
-        #self.reflect(value)
-#        self.parent.control_changed(self.ID, self._value)
-#        return self._value
-
-#    def reflect(self, value):
-#        self._value = clip(self.minval, self.maxval, value)
-#        self.parent.send_to_device(self.ID, self._value)
-
     def configure(self, conf):
-        for k, v in conf.items():
-            setattr(self, k, v)
+        """
+        Configures the control based on the dictionary
+        """
+        for attribute, value in conf.items():
+            try:
+                setattr(self, attribute, value)
+            except AttributeError:
+                eprint("Control of type %s does not have configurable attribute %s!" % (type(self), attribute))
 
     def __repr__(self):
         return "(%i) %i" % (self.ID, self._value)
@@ -63,24 +73,35 @@ class Control(object):
     def __str__(self):
         return self.__repr__()
 
+
 buttonconf = Control.default_conf
 buttonconf.update({"toggle": True})
 
 class Button(Control):
+    """
+    A button can function in toggle or momentary mode. It assumes either Control.minval or Contro.maxval
+    which in most cases will be 0 and 127 respectively.  Apart from those "midi" values we also save
+    the state of the button as a bool.
+
+    For all of this to work, on a "dumb" device you need to set your buttons to momentary. With a momentary
+    hardware button you can emulate a toggle by forcing it on or off.
+    """
 
     # @TODO: This is hacky, make it more straightforward
-    configurable = Control.configurable + ["toggle"]
     default_conf = buttonconf
     
     def __init__(self, ID, parent=None, toggle=True, **kwargs):
         super().__init__(ID, parent, **kwargs)
         self.toggle = toggle
         self.state = False
-        self.ignore = 0
-
-        self.callbacks = []
+        self.ignore = 0  # Used to simulate a toggle behaviour from a momentary button
+                         # To toggle we need to ignore some inputs. This is a countdown
+                         # of how many inputs to ignore before listening again
 
     def on(self):
+        """
+        Simulates activating the button.
+        """
         self._value = self.maxval
         self.state = True
         if self.toggle:
@@ -89,6 +110,9 @@ class Button(Control):
         self.parent.control_changed(self.ID, self.state)
 
     def off(self):
+        """
+        Simulates deactivating the button
+        """
         self._value = self.minval
         self.state = False
         self.ignore = 0
@@ -109,10 +133,10 @@ class Button(Control):
         button is actually momentary.
         """
         # When in a toggle cycle, we need to ignore certain presses:
-        # 1: Turn on
-        # 0: Ignore
-        # 1: Ignore
-        # 0: Turn off
+        # On:  Turn on
+        # Off: Ignore
+        # On:  Ignore
+        # Off: Turn off
         if self.ignore > 0:
             self.ignore -= 1
             self.parent.send_to_device(self.ID, self._value)
@@ -136,6 +160,9 @@ class Button(Control):
             return self._value
     
 class Dial(Control):
+    """
+    A dial spans a range of values (typically 0 to 127, configured using minval and maxval).
+    The hardware control mapped to this could also be a slider or fader, as they technically
+    behave the same, just not in a rotary manner.
+    """
     pass
-
-
--- make_profile.py
+++ make_profile.py
@@ -1,5 +1,13 @@
+"""
+Execute this script with another script as its argument to create an Interface profile.
+
+The called script has to have a function called "create" that takes an Interface object.
+After that script has run the profile is saved to a file with the same name as the script,
+if an ---outfilename is not provided.
+"""
 import argparse
 import importlib
+
 from interface import Interface, BCR2k, VirtualMidi, save_profile
 
 if __name__ == "__main__":

eeef62589bf20625ee82af9ea897449a9d12c3a1
echolox <echolox+bitbucket@gmail.com>
2017-04-26 10:34:43 +0200

    Splits ModView logic into separate methods

--- targets/modview.py
+++ targets/modview.py
@@ -35,8 +35,11 @@ class ModPower(ValueTarget):
 
 class ModView(Target):
     """
+    If the button is held:
     Constructs a temporary view based on the power of a Modifier on the
     Targets in the interface's active view.
+    If the button is pushed momentarily:
+    Constructs a temporary view to configure the modifier itself.
     """
 
     trigger_vals = [0, 127]
@@ -49,27 +52,50 @@ class ModView(Target):
 
     def trigger(self, sender, value=None):
         """
-        Construct and show the temporary view or switch back to the previous one
+        Switch to the mod_config_view or mod_power_view based on how long the button
+        is held/pushed.
         """
         if self.modifier is None:
             self.link_to_mod()
+
         super().trigger(sender, value)
+
+        # Longpress/Shortpres logic
         if value is 127:
             self.prev_view = self.parent.view
-
-            temp_view = View(self.parent.input, name="%s_ModView" % self.modifier)
-
-            for ID, targets in self.prev_view.map.items():
-                for target in targets:
-                    if isinstance(target, ValueTarget) and not isinstance(self.parent.input.controls[ID], Button):
-                        mp = ModPower("%s_%s_PWR" % (target, self.modifier), self.parent, self.modifier, target)
-                        temp_view.map[ID].append(mp)
-
-            ID = next(filter(lambda kv: self in kv[1], self.prev_view.map.items()))[0]
-            temp_view.map[ID].append(self)
-            self.parent.switch_to_view(temp_view, temp=True)
+            self.mod_power_view()
         elif value is 0:
-            self.parent.switch_to_view(self.prev_view)
+            self.go_back()
+    
+    def go_back(self):
+        """
+        Go back to the view that was shown before switching to the mod views.
+        """
+        self.parent.switch_to_view(self.prev_view)
+
+    def mod_config_view(self):
+        """
+        Constructs and shows a view that let's the user configure the modifier attributes
+        itself (amplitude, frequeny ...) with a display of the waveform.
+        """
+        pass
+
+    def mod_power_view(self):
+        """
+        Constructs and shows a view that let's the user adjust the power of the modifier
+        on the targets available in the current view.
+        """
+        temp_view = View(self.parent.input, name="%s_ModView" % self.modifier)
+
+        for ID, targets in self.prev_view.map.items():
+            for target in targets:
+                if isinstance(target, ValueTarget) and not isinstance(self.parent.input.controls[ID], Button):
+                    mp = ModPower("%s_%s_PWR" % (target, self.modifier), self.parent, self.modifier, target)
+                    temp_view.map[ID].append(mp)
+
+        ID = next(filter(lambda kv: self in kv[1], self.prev_view.map.items()))[0]
+        temp_view.map[ID].append(self)
+        self.parent.switch_to_view(temp_view, temp=True)
 
     def serialize(self, *args, **kwargs):
         s = super().serialize(*args, **kwargs) 

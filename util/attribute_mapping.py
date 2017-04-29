e46240a7bae5bf39a0da64e561ff713365aee054
echolox <echolox+bitbucket@gmail.com>
2017-04-29 14:31:44 +0200

    Identifies future issue with mixing data and presentation when declaring AttributeDescriptors on Modifiers

--- util/attribute_mapping.py
+++ util/attribute_mapping.py
@@ -8,6 +8,18 @@ class AttributeType(Enum):
 
 
 AttributeDescriptor = namedtuple("AttributeDescriptor", ["name", "min", "max", "cast", "type", "readonly", "scale"])
+
+""" AttributeGap***
+These object can be used to create an unmapped control.
+
+There is a problem though: When defining the layout of the attributes on the object to be modified, we commit
+to that layout without knowing anything about the control surface/user interface. Right now, we are dealing only
+with the BCR2k.
+
+@TODO: Figure out a way to declare the desired layout of
+- Modifier configs including the step sequencer
+in the profile, so every user can map the controls the way they want it.
+"""
 AttributeGapSpan = AttributeDescriptor("_gap_span", 0, 1, lambda x: x, AttributeType.span, True, None)
 AttributeGapButton = AttributeDescriptor("_gap_boolean", 0, 1, lambda x: x, AttributeType.boolean, True, None)
 
